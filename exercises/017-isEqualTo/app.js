91497e55213967424dbb7908740281c897518d42
gleonag <gleonag@gmail.com>
2022-10-10 19:40:57 +0000

    exercises

--- exercises/017-isEqualTo/app.js
+++ exercises/017-isEqualTo/app.js
@@ -1,1 +1,5 @@
 // Write your function here
+isEqualTo = (num1,num2) => {
+    if (num1 === num2 ) return true
+    else return false 
+}
--- exercises/018-isEven/app.js
+++ exercises/018-isEven/app.js
@@ -1,1 +1,5 @@
 // Write your function here
+isEven = (num) => {
+    if (num % 2 === 0) return true
+    else return false
+}
